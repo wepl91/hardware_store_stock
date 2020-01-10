8e278d3dbc8c76aded4c2008e9dd0215474169be
Walter Pereyra Lopez <walter.pereyra@mercadolibre.com>
2020-01-10 10:27:32 -0300

    Advances in layout

--- src/App.js
+++ src/App.js
@@ -7,18 +7,35 @@ const App = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <Sidebar
-      sidebar={<b>Sidebar content</b>}
+      sidebar={
+        <div className="menu-options-container">
+          <Button className="bp3-minimal" icon="cog" text="Productos" intent="primary" />
+          <Button className="bp3-minimal" icon="list-detail-view" text="Ventas" intent="primary" />
+        </div>
+      }
       open={menuOpen}
       onSetOpen={() => setMenuOpen(!menuOpen)}
       styles={{ sidebar: { background: "white" } }}
     >
       <Navbar>
         <Navbar.Group>
-          <Navbar.Heading>Blueprint</Navbar.Heading>
+          <div className="menu-button-container">
+            <Button
+              className="bp3-minimal"
+              icon={menuOpen ? 'menu-closed' : 'menu-open'}
+              onClick={() => setMenuOpen(!menuOpen)} />
+          </div>
           <Navbar.Divider />
-          <Button className="bp3-minimal" icon="home" text="Home" />
-          <Button className="bp3-minimal" icon="document" text="Files" />
+          <div className="navbar-actions-container">
+            <div class="bp3-input-group .modifier search-input">
+              <span class="bp3-icon bp3-icon-search"></span>
+              <input class="bp3-input" type="search" placeholder="Buscar" dir="auto" />
+            </div>
+          </div>
         </Navbar.Group>
+        <div className="logout-container">
+          <Button className="bp3-minimal" icon="log-out" />
+        </div>
       </Navbar>
     </Sidebar>
   );
