2e38db738690a44ae099657de369001878ed802c
Walter Pereyra Lopez <walter.pereyra@mercadolibre.com>
2020-01-10 16:12:30 -0300

    Products table

--- src/views/products/ProductsList.js
+++ src/views/products/ProductsList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import { Text, H2, Button } from "@blueprintjs/core";
+import ReactDataGrid from 'react-data-grid';
 import './styles.scss'
 
 const ProductsList = (props) => {
   const onClickNewProd = (props) => {
     props.history.push(`${props.history.location.pathname}/new`)
   }
+  const columns = [
+    { key: 'id', name: 'ID' },
+    { key: 'title', name: 'Title' },
+    { key: 'count', name: 'Count' }];
+
+  const rows = [{ id: 0, title: 'row1', count: 20 }, { id: 1, title: 'row1', count: 40 }, { id: 2, title: 'row1', count: 60 }];
+
   return (
     <div className="product-list-container">
       <H2><Text>Listado de productos</Text></H2>
@@ -15,6 +23,11 @@ const ProductsList = (props) => {
         text="Nuevo producto"
         intent="primary"
         onClick={() => onClickNewProd(props)} />
+      <ReactDataGrid
+        columns={columns}
+        rowGetter={i => rows[i]}
+        rowsCount={3}
+        minHeight={150} />
     </div>
   )
 }
