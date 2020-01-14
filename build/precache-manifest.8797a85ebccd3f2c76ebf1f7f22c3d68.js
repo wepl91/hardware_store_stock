621053a3bb7d88362f59786f571a632b1e812696
Walter <w.e.p.l.91@gmail.com>
2020-01-13 21:56:39 -0300

    Redirects in build ??

---
+++ build/precache-manifest.8797a85ebccd3f2c76ebf1f7f22c3d68.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274460958b438c271564b23be705d84b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7849c62e7f7dd0642de8",
+    "url": "/static/css/2.d0f13d07.chunk.css"
+  },
+  {
+    "revision": "cf3527b6262b9342d221",
+    "url": "/static/css/main.4aedcb97.chunk.css"
+  },
+  {
+    "revision": "7849c62e7f7dd0642de8",
+    "url": "/static/js/2.88696a7b.chunk.js"
+  },
+  {
+    "revision": "6cadfdaf69add11d1b7424bcb9b5fadf",
+    "url": "/static/js/2.88696a7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf3527b6262b9342d221",
+    "url": "/static/js/main.5a69af07.chunk.js"
+  },
+  {
+    "revision": "274cf5d3c2503767dd9e",
+    "url": "/static/js/runtime-main.3e68b4e9.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
